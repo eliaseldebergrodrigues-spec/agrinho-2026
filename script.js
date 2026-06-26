fb06a006f5e7997116150ba532ee162e56566e9a
eliaseldebergrodrigues-spec <elias.eldeberg.rodrigues@escola.pr.gov.br>
2026-06-26 10:05:22 -0300

    asdasd

---
+++ script.js
@@ -0,0 +1,3 @@
+function mostrarDica() {
+    alert("Dica Importante: Sabia que antenas repetidoras de sinal de celular de baixo custo já ajudam muitos produtores a conseguir conexão estável em pontos específicos da fazenda?");
+}
